25bbf38116b96149bb11236639c062521eae35ae
yara89 <60362934+yara89@users.noreply.github.com>
2021-02-14 20:09:35 +0100

    Create palindrome.py

---
+++ Python/palindrome.py
@@ -0,0 +1,9 @@
+# input 
+s=input("enter word or numbers: ") 
+# reverse of a string
+a=s[::-1]
+# print results if input word is equal or not
+if (a==s):
+    print("is Palindrome")
+else:
+    print("is not Palindrome")
