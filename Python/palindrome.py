eb6e68baaca55a77e0422ab14d5e6437634da3ad
yara89 <60362934+yara89@users.noreply.github.com>
2021-02-17 16:57:37 +0100

    Delete palindrome.py

--- Python/palindrome.py
+++
@@ -1,9 +0,0 @@
-# input 
-s=input("enter word or numbers: ") 
-# reverse of a string
-a=s[::-1]
-# print results if input word is equal or not
-if (a==s):
-    print("is Palindrome")
-else:
-    print("is not Palindrome")
