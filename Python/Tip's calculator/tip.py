937bdafe95edb3cfceef3fc2b08313f49763aec7
yara89 <60362934+yara89@users.noreply.github.com>
2021-02-17 16:53:52 +0100

    Create tip.py
    Tip calculator in python with total tip per guest

---
+++ Python/Tip's calculator/tip.py
@@ -0,0 +1,12 @@
+# tip calculator
+
+bill = 80
+guests = 2
+percentage = 10
+
+# the bill multiplied by percentage
+tip = bill * (percentage / 100)
+# total is divided by number of guests
+total = tip / guests
+
+print(total, 'tip per guest')
