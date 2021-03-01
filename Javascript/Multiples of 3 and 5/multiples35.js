9ae04590efd81a364d13b0ee290e3e2288294ce1
yara89 <60362934+yara89@users.noreply.github.com>
2021-03-01 19:28:24 +0100

    Update multiples35.js

--- Javascript/Multiples of 3 and 5/multiples35.js
+++ Javascript/Multiples of 3 and 5/multiples35.js
@@ -1,3 +1,5 @@
+// list all the numbers between 1 and 1000
+// that are multiples of 3 or 5
 function multiples(number) {
   let sum = 0;
   for (let i = 0; i <= number; i++) {
@@ -12,3 +14,4 @@ function multiples(number) {
 console.log( multiples(10) );
 console.log( multiples(5) );
 console.log( multiples(6) );
+
