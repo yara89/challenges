03a8ea75098716c8c07e02dc5c7fd0d98e391909
yara89 <60362934+yara89@users.noreply.github.com>
2021-03-03 13:23:20 +0100

    Update multiples.js

--- Javascript/Multiples of 3 and 5/multiples.js
+++ Javascript/Multiples of 3 and 5/multiples.js
@@ -1,16 +1,17 @@
+// list all the numbers between 1 and 1000 that are multiples of 3 or 5
 function multiples() {
     let sum = 0;
     for (let i = 1; i < 1000; i++) {
-  
+
       if (i % 3 === 0 || i % 5 === 0) {
         sum += i;
       }
     }
     return sum;
   };
-  
+
   console.log(multiples());
 
-  // sum is 233168... 
+  // sum is 233168...
   //the same is returned wether  && or || is used, not sure which is more accurate.
   // is the 1000 is to be included in the sum, it should be i <= 1000
